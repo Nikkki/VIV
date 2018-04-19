f8d986a711306918ac599bb150654db907239772
Nick <nikikier1@gmail.com>
2018-04-20 01:43:48 +0300

    added all questions to project

---
+++ src/store/questionsOld.js
@@ -0,0 +1,459 @@
+[{
+    numb: 14,
+    question: 'Что такое грех?',
+    answer: 'Грех — это любой недостаток послушания закону Божию или нарушение его(1).',
+    sups: {
+      1: [{
+        text: '1 Ин. 3:4',
+        link: 'https://allbible.info/bible/sinodal/1jo/3#4'
+      }]
+    }
+  },
+  {
+    numb: 15,
+    question: 'Что являлось грехом, из-за которого наши прародители пали из ' +
+      ' того состояния, в котором они были созданы?',
+    answer: 'Грех, из-за которого наши прародители пали из состояния, ' +
+      ' в котором они были созданы, заключался в том, что они вкусили запретных плодов(1).',
+    sups: {
+      1: [{
+        text: 'Быт. 3:6,12',
+        link: 'http://allbible.info/bible/sinodal/ge/3#6'
+      }]
+    }
+  },
+  {
+    numb: 16,
+    question: 'Всё ли человечество пало в первом согрешении Адама?',
+    answer: 'Так как завет был заключён с Адамом не только для него самого, но и для его потомства, то все человечество, происходя от него из поколения в поколение, согрешило в нём и пало вместе с ним в его первом грехе(1).',
+    sups: {
+      1: [{
+          text: 'Быт. 2:16-17',
+          link: 'http://allbible.info/bible/sinodal/ge/2#16'
+        },
+        {
+          text: 'Рим. 5:12',
+          link: 'http://allbible.info/bible/sinodal/ro/5#12'
+        },
+        {
+          text: '1 Кор. 15:21-22',
+          link: 'http://allbible.info/bible/sinodal/1co/15#21'
+        }
+      ]
+    }
+  },
+  {
+    numb: 17,
+    question: 'В какое состояние грехопадение привело человечество?',
+    answer: 'Грехопадение привело человечество в состояние греха и страдания(1).',
+    sups: {
+      1: [{
+        text: 'Рим. 5:12',
+        link: 'http://allbible.info/bible/sinodal/ro/5#12'
+      }]
+    }
+  },
+  {
+    numb: 18,
+    question: 'В чём заключается греховность того состояния, в которое впал человек?',
+    answer: 'Греховность состояния, в которое впал человек, заключается в вине ' +
+      ' за первый грех Адама, в отсутствии первоначальной праведности и испорченности ' +
+      ' всей его природы, что обычно называется первородным грехом, вместе со всеми ' +
+      'действительными прегрешениями, проистекающими от него(1).',
+    sups: {
+      1: [{
+          text: 'Рим. 5:12,19',
+          link: 'http://allbible.info/bible/sinodal/ro/5#12'
+        }, {
+          text: 'Рим. 5:10-20',
+          link: 'http://allbible.info/bible/sinodal/ro/5#10'
+        },
+        {
+          text: 'Еф. 2:1-3',
+          link: 'http://allbible.info/bible/sinodal/eph/2#1'
+        },
+        {
+          text: 'Иак. 1:14,15',
+          link: 'http://allbible.info/bible/sinodal/jas/1#14'
+        }, {
+          text: 'Мф. 15:19',
+          link: 'http://allbible.info/bible/sinodal/mt/15#19'
+        }
+      ]
+    }
+  },
+  {
+    numb: 19,
+    question: 'В чём заключается несчастье того состояния, в которое впал человек?',
+    answer: 'Всё человечество через грехопадение потеряло общение с Богом(1), ' +
+      'оно находится под Его гневом и проклятием(2), и стало, таким образом, подвержено ' +
+      'всем скорбям в этой жизни, самой смерти и вечным мукам ада(3).',
+    sups: {
+      1: [{
+        text: 'Быт. 3:8,10,24',
+        link: 'http://allbible.info/bible/sinodal/ge/3#8'
+      }],
+      2: [{
+          text: 'Еф. 2:2-3',
+          link: 'http://allbible.info/bible/sinodal/eph/2#2'
+        },
+        {
+          text: 'Гал. 3:10',
+          link: 'http://allbible.info/bible/sinodal/ga/3#10'
+        }
+      ],
+      3: [{
+          text: 'Плач Иер. 3:39',
+          link: 'http://allbible.info/bible/sinodal/la/3#39'
+        },
+        {
+          text: 'Рим. 6:23',
+          link: 'http://allbible.info/bible/sinodal/ro/3#23'
+        },
+        {
+          text: 'Мф. 25:41,46',
+          link: 'http://allbible.info/bible/sinodal/mt/25#41'
+        }
+      ]
+    }
+  },
+  {
+    numb: 20,
+    question: 'Оставил ли Бог всё человечество погибать в состоянии греха и страдания?',
+    answer: 'По Своей благой воле Бог избрал от вечности некоторых для вечной жизни(1) ' +
+      ' и вступил с ними в завет благодати, чтобы избавить их от состояния греха и ' +
+      'страдания и привести их в состояние спасения через Искупителя(2).',
+    sups: {
+      1: [{
+        text: 'Еф. 1:4',
+        link: 'http://allbible.info/bible/sinodal/eph/1#4'
+      }],
+      2: [{
+          text: 'Рим. 3:20-22',
+          link: 'http://allbible.info/bible/sinodal/ro/3#20'
+        },
+        {
+          text: 'Гал. 3:21-22',
+          link: 'http://allbible.info/bible/sinodal/ga/3#21'
+        }
+      ]
+    }
+  },
+  {
+    numb: 21,
+    question: 'Кто является Искупителем избранных Божиих?',
+    answer: 'Единственный Искупитель избранных Божиих — ' +
+      'это Господь Иисус Христос(1), Который, будучи вечным Сыном Божиим,' +
+      'стал человеком(2), и таким образом Он был и остаётся Богом и человеком' +
+      'в двух различных природах, но одной Личности, вовеки(3).',
+    sups: {
+      1: [{
+        text: '1 Тим. 2:5,6',
+        link: 'http://allbible.info/bible/sinodal/1ti/2#5'
+      }],
+      2: [{
+        text: 'Ин. 1:14',
+        link: 'http://allbible.info/bible/sinodal/joh/1#14'
+      }],
+      3: [{
+          text: 'Рим. 9:5',
+          link: 'http://allbible.info/bible/sinodal/ro/9#5'
+        },
+        {
+          text: 'Лк. 1:35',
+          link: 'http://allbible.info/bible/sinodal/lu/1#35'
+        },
+        {
+          text: 'Евр. 7:24-25',
+          link: 'http://allbible.info/bible/sinodal/heb/7#24'
+        }
+      ]
+    }
+  },
+  {
+    numb: 22,
+    question: 'Каким образом Христос, будучи Сыном Божиим, стал человеком?',
+    answer: 'Христос, Сын Божий, стал человеком, восприняв на Себя подлинное ' +
+      'тело(1) и разумную душу(2), будучи зачат силой Святого Духа во чреве девы ' +
+      'Марии и рождённым от неё(3), всё же без греха(4).',
+    sups: {
+      1: [{
+        text: 'Евр. 2:14,16',
+        link: 'http://allbible.info/bible/sinodal/heb/2#14'
+      }, {
+        text: 'Евр. 10:5',
+        link: 'http://allbible.info/bible/sinodal/heb/10#10'
+      }],
+      2: [{
+        text: 'Мф. 26:38',
+        link: 'http://allbible.info/bible/sinodal/mt/26#28'
+      }],
+      3: [{
+          text: 'Лука 1:27,31,35,42',
+          link: 'http://allbible.info/bible/sinodal/lu/1#27'
+        },
+        {
+          text: 'Гал. 4:4',
+          link: 'http://allbible.info/bible/sinodal/ga/4#4'
+        }
+      ],
+      4: [{
+          text: 'Евр. 4:15',
+          link: 'http://allbible.info/bible/sinodal/heb/4#15'
+        },
+        {
+          text: 'Евр. 7:26',
+          link: 'http://allbible.info/bible/sinodal/heb/7#26'
+        }
+      ]
+    }
+  },
+  {
+    numb: 23,
+    question: 'Какие служения исполняет Христос как наш Искупитель?',
+    answer: 'Как наш Искупитель, Христос исполняет служения пророка, ' +
+      'священника и царя, как в состоянии уничижения, так и в состоянии возвеличения(1).',
+    sups: {
+      1: [{
+          text: 'Деян. 3:21-22',
+          link: 'http://allbible.info/bible/sinodal/ac/3#21'
+        },
+        {
+          text: 'Евр. 12:25',
+          link: 'http://allbible.info/bible/sinodal/heb/12#25'
+        },
+        {
+          text: 'сравни с 1 Кор. 13:3',
+          link: 'http://allbible.info/bible/sinodal/1co/13#3'
+        },
+        {
+          text: 'Евр. 5:5-7',
+          link: 'http://allbible.info/bible/sinodal/heb/5#5'
+        },
+        {
+          text: 'Евр. 7:25',
+          link: 'http://allbible.info/bible/sinodal/heb/7#25'
+        },
+        {
+          text: 'Пс. 2:6',
+          link: 'http://allbible.info/bible/sinodal/ps/2#6'
+        },
+        {
+          text: 'Ис. 9:6-7',
+          link: 'http://allbible.info/bible/sinodal/isa/9#6'
+        },
+        {
+          text: 'Мф. 21:5 ',
+          link: 'http://allbible.info/bible/sinodal/mt/21#5'
+        },
+        {
+          text: 'Пс. 2:8-11',
+          link: 'http://allbible.info/bible/sinodal/ps/2#8'
+        }
+      ]
+    }
+  },
+  {
+    numb: 24,
+    question: 'Как Христос несёт служение пророка?',
+    answer: 'Христос несёт служение пророка, открывая нам волю Божию для нашего спасения посредством Своего Слова и Духа(1).',
+    sups: {
+      1: [{
+          text: 'Ин. 1:18',
+          link: 'http://allbible.info/bible/sinodal/joh/1#18'
+        }, {
+          text: '1 Петр. 1:10-12',
+          link: 'http://allbible.info/bible/sinodal/1pe/1#10'
+        }, {
+          text: 'Ин. 15:15',
+          link: 'http://allbible.info/bible/sinodal/joh/15#15'
+        },
+        {
+          text: 'Ин. 20:31',
+          link: 'http://allbible.info/bible/sinodal/joh/20#31'
+        },
+      ]
+    }
+  },
+  {
+    numb: 25,
+    question: 'Как Христос несёт служение священника?',
+    answer: 'Христос несёт служение священника, однажды принеся Самого Себя в жертву, чтобы удовлетворить Божественную справедливость(1) и примирить нас с Богом(2); и постоянно ходатайствуя за нас(3).',
+    sups: {
+      1: [{
+        text: 'Евр. 9:14,28',
+        link: 'http://allbible.info/bible/sinodal/heb/9#14'
+      }],
+      2: [{
+        text: 'Евр. 2:17',
+        link: 'http://allbible.info/bible/sinodal/heb/2#17'
+      }],
+      3: [{
+        text: 'Евр. 7:24,25',
+        link: 'http://allbible.info/bible/sinodal/heb/7#24'
+      }]
+    }
+  },
+  {
+    numb: 26,
+    question: 'Как Христос несёт служение царя?',
+    answer: 'Христос несёт служение царя, покоряя нас Себе(1) управляя нами(2) и защищая нас(3), сдерживая и побеждая всех Своих и наших врагов(4).',
+    sups: {
+      1: [{
+        text: 'Деян. 15:14-16',
+        link: 'http://allbible.info/bible/sinodal/ac/15#14'
+      }],
+      2: [{
+        text: 'Ис. 33:22',
+        link: 'http://allbible.info/bible/sinodal/isa/33#22'
+      }],
+      3: [{
+        text: 'Ис. 32:1-2',
+        link: 'http://allbible.info/bible/sinodal/isa/32#1'
+      }],
+      4: [{
+          text: '1 Кор. 15:25',
+          link: 'http://allbible.info/bible/sinodal/1co/15#25'
+        },
+        {
+          text: 'Пс. 109',
+          link: 'http://allbible.info/bible/sinodal/ps/109#1'
+        }
+      ],
+    }
+  },
+  {
+    numb: 27,
+    question: 'В чём заключалось уничижение Христа?',
+    answer: 'Уничижение Христа заключалось в том, что Он был рождён в ' +
+      'низком социальном положении(1), подчинился закону(2), испытал страдания' +
+      'этой жизни(3), гнев Божий(4) и позорную смерть на кресте(5); ' +
+      'Он был погребён(6) и некоторое время находился во власти смерти(7).',
+    sups: {
+      1: [{
+        text: 'Лк. 2:7',
+        link: 'http://allbible.info/bible/sinodal/lu/2#7'
+      }],
+      2: [{
+        text: 'Гал. 4:4',
+        link: 'http://allbible.info/bible/sinodal/eph/4#4'
+      }],
+      3: [{
+          text: 'Евр. 12:2-3',
+          link: 'http://allbible.info/bible/sinodal/heb/12#2'
+        },
+        {
+          text: 'Ис. 53:2-3',
+          link: 'http://allbible.info/bible/sinodal/isa/53#2'
+        }
+      ],
+      4: [{
+          text: 'Лк. 22:44',
+          link: 'http://allbible.info/bible/sinodal/lu/22#44'
+        },
+        {
+          text: 'Мф. 27:46',
+          link: 'http://allbible.info/bible/sinodal/mt/27#46'
+        }
+      ],
+      5: [{
+        text: 'Фил. 2:7,8',
+        link: 'http://allbible.info/bible/sinodal/php/2#7'
+      }],
+      6: [{
+        text: '1 Кор. 15:3-4',
+        link: 'http://allbible.info/bible/sinodal/1co/15#3'
+      }],
+      7: [{
+        text: 'Деян. 2:24-27,31',
+        link: 'http://allbible.info/bible/sinodal/ac/2#24'
+      }]
+    }
+  },
+  {
+    numb: 28,
+    question: 'В чём заключается возвеличение Христа?',
+    answer: 'Возвеличение Христа заключается в том, что Он воскрес из мёртвых ' +
+      'на третий день(1), вознёсся на небо(2), сел одесную Бога Отца(3) и грядёт, ' +
+      'чтобы судить мир в последний день(4).',
+    sups: {
+      1: [{
+        text: '1 Кор. 15:4',
+        link: 'http://allbible.info/bible/sinodal/1co/15#4'
+      }],
+      2: [{
+        text: 'Мк. 16:19',
+        link: 'http://allbible.info/bible/sinodal/mr/16#19'
+      }],
+      3: [{
+        text: 'Еф. 1:20',
+        link: 'http://allbible.info/bible/sinodal/eph/1#20'
+      }],
+      4: [{
+        text: 'Деян. 1:11',
+        link: 'http://allbible.info/bible/sinodal/ac/1#11'
+      }, {
+        text: 'Деян. 17:31',
+        link: 'http://allbible.info/bible/sinodal/ac/17#31'
+      }],
+    }
+  },
+  {
+    numb: 29,
+    question: 'Каким образом мы становимся причастниками искупления, приобретённого Христом?',
+    answer: 'Мы становимся причастниками искупления, приобретённого Христом, ' +
+      'через действенное приложение его к нам(1) Его Святым Духом(2).',
+    sups: {
+      1: [{
+        text: 'Ин. 1:11-12',
+        link: 'http://allbible.info/bible/sinodal/joh/1#11'
+      }],
+      2: [{
+        text: 'Титу 3:5-6',
+        link: 'http://allbible.info/bible/sinodal/tit/3#5'
+      }]
+    }
+  },
+  {
+    numb: 30,
+    question: 'Как Дух прилагает к нам искупление, приобретённое Христом?',
+    answer: 'Дух прилагает к нам искупление, приобретённое Христом, ' +
+      'производя в нас веру(1), и таким образом соединяя нас со Христом ' +
+      'в нашем действенном призвании(2).',
+    sups: {
+      1: [{
+          text: 'Еф. 1:13-14',
+          link: 'http://allbible.info/bible/sinodal/eph/1#13'
+        },
+        {
+          text: 'Ин. 6:37,39',
+          link: 'http://allbible.info/bible/sinodal/joh/6#37'
+        },
+        {
+          text: 'Еф. 2:8',
+          link: 'http://allbible.info/bible/sinodal/eph/2#8'
+        }
+      ],
+      2: [{
+          text: 'Еф. 3:17,18',
+          link: 'http://allbible.info/bible/sinodal/eph/3#17'
+        },
+        {
+          text: '1 Кор. 1:9',
+          link: 'http://allbible.info/bible/sinodal/1co/1#9'
+        }
+      ]
+    }
+  },
+  // {
+  //   numb: 31,
+  //   question: '',
+  //   answer: '',
+  //   sups: {
+  //     1: [{
+  //       text: '',
+  //       link: ''
+  //     }]
+  //   }
+  // }
+]
